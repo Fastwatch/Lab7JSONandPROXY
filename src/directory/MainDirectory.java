cd7d6a0498f281824c808d0e5e46754802051ee0
fher <fher@ceas-942-mac11.cae.uwm.edu>
2018-03-06 12:39:12 -0600

    Push

---
+++ src/directory/MainDirectory.java
@@ -0,0 +1,52 @@
+package directory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+public class MainDirectory implements Directory{
+
+	private ArrayList<Employee> employeeList = new ArrayList<Employee>();
+	
+	@Override
+	public void print() {
+		if(employeeList.isEmpty()) {
+			System.out.println("Empty directory.");
+			return;
+		}
+		for (Employee pp: employeeList) {
+			System.out.println(pp.toString());
+		}
+	}
+
+	@Override
+	public void add(String s) {
+		Gson g = new Gson();
+		ArrayList<Employee> ep = (g.fromJson(s, new TypeToken<Collection<Employee>>(){}.getType()));
+		for (Employee pp: ep) {
+			employeeList.add(pp);
+		}
+		 Collections.sort(employeeList);
+	}
+
+	@Override
+	public void clr() {
+		employeeList.clear();		
+	}
+	
+	public static class Comparators {
+
+        public static Comparator<Employee> NAME = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                return e1.compareTo(e2);
+            }
+        };
+	}
+
+}
