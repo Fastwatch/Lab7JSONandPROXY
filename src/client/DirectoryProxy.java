33c9d771c7f5a94743dbb6a72e047f44d24702ec
Riley Mahr <rwmahr@gmail.com>
2018-03-06 11:56:35 -0600

    client classes

---
+++ src/client/DirectoryProxy.java
@@ -0,0 +1,25 @@
+package client;
+
+import directory.*;
+
+public class DirectoryProxy implements Directory {
+
+	@Override
+	public String print() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
