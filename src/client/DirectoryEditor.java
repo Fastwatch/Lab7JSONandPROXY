c43421e50af4abfe3651e2b8430381a2bb2b6bd6
Riley Mahr <rwmahr@gmail.com>
2018-03-06 12:04:56 -0600

    adding base class for dir editor

--- src/client/DirectoryEditor.java
+++ src/client/DirectoryEditor.java
@@ -1,5 +1,39 @@
 package client;
 
-public class DirectoryEditor {
+import java.util.Scanner;
 
+public class DirectoryEditor {
+	
+	private static DirectoryProxy dirp;
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String response;
+		
+		do {
+			System.out.println("File (F) or Console (C) Input, or Exit (E)?");
+			response = in.nextLine();
+			switch(response.toUpperCase()) {
+			case "F":
+				fileInput();
+				break;
+			case "C":
+				consoleInput();
+				break;
+			}
+			
+		}while(!response.equalsIgnoreCase("e"));
+		
+		in.close();
+	}
+	
+	private static void consoleInput() {
+		
+	}
+	
+	private static void fileInput() {
+		
+	}
+	
+	
 }
